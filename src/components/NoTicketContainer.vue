f922d73505ee0252448bd280153595ab3622c88c
Vanessa Ejikeme <vanessa.ejikeme@gmail.com>
2020-07-19 11:53:42 +0100

    Update NoTicket nav path

--- src/components/NoTicketContainer.vue
+++ src/components/NoTicketContainer.vue
@@ -1,15 +1,20 @@
 <template>
-  <form v-if="!formSubmitted">
-    <label for="email">Enter your email address here</label>
-    <input type="text" name="email-address"/> 
-  </form>
+  <div>
+    <form v-if="!formSubmitted">
+      <label for="email">Enter your email address here</label>
+      <input type="text" name="email-address"/> 
+    </form>
+    <div v-else>
+      <p>Ticket sent</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      formSubmitted: false
+      formSubmitted: true
     }
   }
 }
