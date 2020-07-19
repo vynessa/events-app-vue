29becac7055d2d9a760fcd3d45538118e183a3f7
Vanessa Ejikeme <vanessa.ejikeme@gmail.com>
2020-07-19 09:31:19 +0100

    Implement events list and detail functionality - integrate pages with api data and matching accurate route on click of an event on list - set event in localstorage - add payment page navigation from event detail

--- src/components/EventDetailContainer.vue
+++ src/components/EventDetailContainer.vue
@@ -1,60 +1,73 @@
 <template>
-  <div class="event-detail">
-    <div class="event-detail__info event-">
-      <h6 class="event-detail__info-date">8TH FEBRUARY 2019</h6>
-      <h4 class="event-detail__info-title">{{event.name}}</h4>
-      <p>{{event.description}}</p>
-      <p class="event-detail__info-price">N5,000 - N1,000,000</p>
-      <button class="event-detail__register-btn">Buy Tickets | REGISTER for free</button>
-    </div>
+  <div>
+    <p v-if="!event">...loading</p>
 
-    <div class="event-detail__info event-flyer">
-      <img class="event-detail__info-img" src="../assets/anthony-unsplash.jpg">
-    </div>
+    <div v-else class="event-detail">
+      <div class="event-detail__info event-">
+        <h6 class="event-detail__info-date">{{displayParsedTime(event.start_time)}}</h6>
+        <h4 class="event-detail__info-title">{{event.name}}</h4>
+        <p>{{event.description}}</p>
+        <p class="event-detail__info-price">N5,000 - N1,000,000</p>
+        <button @click="handleClick(event.is_free)" class="event-detail__register-btn">{{event.is_free ? "REGISTER for free": "Buy Tickets"}}</button>
+      </div>
 
-    <hr class="horizontal-line">
+      <div class="event-detail__info event-flyer">
+        <img class="event-detail__info-img" :src="event.image || `../assets/anthony-unsplash.jpg`">
+      </div>
 
-    <div class="event-detail__info event-venue">
-      <h6 class="event-detail__info-venue-text">Venue</h6>
-      <h4 class="event-detail__info-venue">{{event.venue}}</h4>
-      <a href="" class="navbar-links">
-        View map for directions
-      </a>
-    </div>
-    <div class="event-detail__info event-date-time">
-      <h6 class="event-detail__info-date">Date and time</h6>
-      <div class="event-detail__social-links"> 
-        <p class="navbar-links">https://www.nath.com</p>
-        <p class="navbar-links">https://www.twitter.com/nath</p>
-        <p class="navbar-links">https://www.instagram.com/nath</p>
-      </div> 
+      <hr class="horizontal-line">
+
+      <div class="event-detail__info event-venue">
+        <h6 class="event-detail__info-venue-text">Venue</h6>
+        <h4 class="event-detail__info-venue">{{event.venue}}</h4>
+        <a href="" class="navbar-links">
+          View map for directions
+        </a>
+      </div>
+      <div class="event-detail__info event-date-time">
+        <h6 class="event-detail__info-date">Date and time</h6>
+        <div class="event-detail__social-links" v-if="event.social_links || event.social_links.length > 0"> 
+          <p class="navbar-links">https://www.nath.com</p>
+          <p class="navbar-links">https://www.twitter.com/nath</p>
+          <p class="navbar-links">https://www.instagram.com/nath</p>
+        </div> 
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import MainContainer from '../components/MainContainer';
+import EventsApi from '../services/api';
+import { parseTime } from '../services/utils';
 
 export default {
   data () {
     return {
-      event: {
-        createdAt: "2020-04-30T10:51:37.000Z",
-        description: "For rappers",
-        end_time: "2020-08-21T00:00:00.000Z",
-        id: 1,
-        image: null,
-        is_free: false,
-        is_sold_out: false,
-        name: "Lyricist Lounge",
-        num_of_tickets: 10000,
-        social_links: "{}",
-        start_time: "2020-08-21T00:00:00.000Z",
-        tags: "Music",
-        tickets_sale_end_date: "2020-08-21T00:00:00.000Z",
-        updatedAt: "2020-04-30T10:51:37.000Z",
-        venue: "Lagos"
-      }
+      loading: false,
+      event: null,
+      showRegisterModal: false
+    }
+  },
+  created() {
+    this.getEventDetail()
+  },
+  methods: {
+    getEventDetail() {
+      this.loading = true;
+      EventsApi.getEventDetail(this.$route.params.id).then((response) => {
+        this.event = response.data
+      })
+    },
+    setEvent(){
+      localStorage.setItem("selectedEvent", JSON.stringify(this.event));
+      this.$router.push('/payment-page');
+    },
+    handleClick(isFree){
+      isFree ? this.showRegisterModal : this.setEvent()
+    },
+    displayParsedTime(time) {
+      return parseTime(time)
     }
   },
   components: {
--- src/components/EventsListContainer.vue
+++ src/components/EventsListContainer.vue
@@ -4,161 +4,66 @@
       {{text}}
     </h1>
     <div class="events-list" > 
-      <div class="events-list__item" v-for="event in events" :key="event.id">
-        <img class="events-list__img" :src="event.image || `../assets/anthony-unsplash.jpg`">
-        <h6 class="events-list__date"  >8TH FEBRUARY 2019</h6>
+      <p v-if="loading">........Loading</p>
+      <div class="events-list__item" v-for="event in eventsDataList.events" :key="event.id">
+        <img v-on:click="displayEvent(event.id)" class="events-list__img" :src="event.image || `../assets/anthony-unsplash.jpg`">
+        <h6 class="events-list__date">{{displayParsedTime(event.start_time)}}</h6>
         <h4 class="events-list__title">{{event.name}}</h4>
-        <p class="events-list__price">N5,000 - N1,000,000</p>
+        <p class="events-list__price">{{getEventType(event.is_free, event.is_sold_out)}}</p>
       </div>
     </div>
   </div>  
 </template>
 
 <script>
+import EventsApi from "../services/api"
+import { parseTime } from '../services/utils';
+
 export default {
-  data () {
+   data () {
     return {
       text: "The best events happening now.",
-      events: [
-      {
-        createdAt: "2020-04-30T10:51:37.000Z",
-        description: "For rappers",
-        end_time: "2020-08-21T00:00:00.000Z",
-        id: 1,
-        image: null,
-        is_free: false,
-        is_sold_out: false,
-        name: "Lyricist Lounge",
-        num_of_tickets: 10000,
-        social_links: "{}",
-        start_time: "2020-08-21T00:00:00.000Z",
-        tags: "Music",
-        tickets_sale_end_date: "2020-08-21T00:00:00.000Z",
-        updatedAt: "2020-04-30T10:51:37.000Z",
-        venue: "Lagos"
-      },
-      {
-        createdAt: "2020-04-30T10:51:37.000Z",
-        description: "For rappers",
-        end_time: "2020-08-21T00:00:00.000Z",
-        id: 2,
-        image: null,
-        is_free: false,
-        is_sold_out: false,
-        name: "Lyricist Lounge",
-        num_of_tickets: 10000,
-        social_links: "{}",
-        start_time: "2020-08-21T00:00:00.000Z",
-        tags: "Music",
-        tickets_sale_end_date: "2020-08-21T00:00:00.000Z",
-        updatedAt: "2020-04-30T10:51:37.000Z",
-        venue: "Lagos"
-      },
-      {
-        createdAt: "2020-04-30T10:51:37.000Z",
-        description: "For rappers",
-        end_time: "2020-08-21T00:00:00.000Z",
-        id: 3,
-        image: null,
-        is_free: false,
-        is_sold_out: false,
-        name: "Lyricist Lounge",
-        num_of_tickets: 10000,
-        social_links: "{}",
-        start_time: "2020-08-21T00:00:00.000Z",
-        tags: "Music",
-        tickets_sale_end_date: "2020-08-21T00:00:00.000Z",
-        updatedAt: "2020-04-30T10:51:37.000Z",
-        venue: "Lagos"
-      },
-      {
-        createdAt: "2020-04-30T10:51:37.000Z",
-        description: "For rappers",
-        end_time: "2020-08-21T00:00:00.000Z",
-        id: 4,
-        image: null,
-        is_free: false,
-        is_sold_out: false,
-        name: "Lyricist Lounge",
-        num_of_tickets: 10000,
-        social_links: "{}",
-        start_time: "2020-08-21T00:00:00.000Z",
-        tags: "Music",
-        tickets_sale_end_date: "2020-08-21T00:00:00.000Z",
-        updatedAt: "2020-04-30T10:51:37.000Z",
-        venue: "Lagos"
-      },
-      {
-        createdAt: "2020-04-30T10:51:37.000Z",
-        description: "For rappers",
-        end_time: "2020-08-21T00:00:00.000Z",
-        id: 5,
-        image: null,
-        is_free: false,
-        is_sold_out: false,
-        name: "Lyricist Lounge",
-        num_of_tickets: 10000,
-        social_links: "{}",
-        start_time: "2020-08-21T00:00:00.000Z",
-        tags: "Music",
-        tickets_sale_end_date: "2020-08-21T00:00:00.000Z",
-        updatedAt: "2020-04-30T10:51:37.000Z",
-        venue: "Lagos"
-      },
-      {
-        createdAt: "2020-04-30T10:51:37.000Z",
-        description: "For rappers",
-        end_time: "2020-08-21T00:00:00.000Z",
-        id: 6,
-        image: null,
-        is_free: false,
-        is_sold_out: false,
-        name: "Lyricist Lounge",
-        num_of_tickets: 10000,
-        social_links: "{}",
-        start_time: "2020-08-21T00:00:00.000Z",
-        tags: "Music",
-        tickets_sale_end_date: "2020-08-21T00:00:00.000Z",
-        updatedAt: "2020-04-30T10:51:37.000Z",
-        venue: "Lagos"
-      },
-      {
-        createdAt: "2020-04-30T10:51:37.000Z",
-        description: "For rappers",
-        end_time: "2020-08-21T00:00:00.000Z",
-        id: 7,
-        image: null,
-        is_free: false,
-        is_sold_out: false,
-        name: "Lyricist Lounge",
-        num_of_tickets: 10000,
-        social_links: "{}",
-        start_time: "2020-08-21T00:00:00.000Z",
-        tags: "Music",
-        tickets_sale_end_date: "2020-08-21T00:00:00.000Z",
-        updatedAt: "2020-04-30T10:51:37.000Z",
-        venue: "Lagos"
-      },
-      {
-        createdAt: "2020-04-30T10:51:37.000Z",
-        description: "For rappers",
-        end_time: "2020-08-21T00:00:00.000Z",
-        id: 8,
-        image: null,
-        is_free: false,
-        is_sold_out: false,
-        name: "Lyricist Lounge",
-        num_of_tickets: 10000,
-        social_links: "{}",
-        start_time: "2020-08-21T00:00:00.000Z",
-        tags: "Music",
-        tickets_sale_end_date: "2020-08-21T00:00:00.000Z",
-        updatedAt: "2020-04-30T10:51:37.000Z",
-        venue: "Lagos"
+      loading: false,
+      eventsDataList: [],
+    }
+  },
+  created() {
+    this.getEvents;
+  },
+  computed: {
+     getEvents() {
+      this.loading = true;
+      EventsApi.getEvents().then((response) => {
+        this.eventsDataList = response.data
+        this.loading = false;
+      });
+    },
+  },
+  methods: {
+    // getEvents() {
+    //   this.loading = true;
+    //   EventsApi.getEvents().then((response) => {
+    //     this.eventsDataList = response.data
+    //     this.loading = false;
+    //   });
+    // },
+    displayEvent(id) {
+      this.$router.push(`/events/${id}`)
+    },
+    displayParsedTime(time) {
+      return parseTime(time)
+    },
+    getEventType(freeEvent, status){
+      if (freeEvent && !status){
+        return "Free"
       }
-    ]
+      if(!freeEvent && !status){
+        return "N5000 - N50000000"
+      }
+      if(status){
+        return "Sold Out"
+      }
     }
   }
 }
 </script>
-
