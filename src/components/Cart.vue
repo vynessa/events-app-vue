e914dc4a830fe20095a3fa7236c1454707286693
Vanessa Ejikeme <vanessa.ejikeme@gmail.com>
2020-07-19 10:09:16 +0100

    Implement cart management functionality - section components into OrderSummary and Cart - section Cart into TicketType - emit events to add and remove from Cart and update OrderSummary

---
+++ src/components/Cart.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="event-payment-page">
+      <button class="close-payment-page">x Close</button>
+      <header>
+        <h4 class="event-detail__info-title">{{event.name}}</h4>
+        <h6 class="event-detail__info-date">8TH FEBRUARY 2019</h6>
+      </header>
+
+      <div class="event-ticket-category">
+        <ticket-type 
+
+          v-for="eventTicketType in eventTicketTypes"
+          :eventTicketType="eventTicketType"
+          :key="eventTicketType.id"
+          :add-to-cart="addToCart"
+          :remove-from-cart="removeFromCart"
+        >
+        </ticket-type>
+        <p>Ticket sale ends on 22nd November 2019</p>
+      </div>
+    </div>
+</template>
+
+<script>
+import TicketType from '../components/TicketType';
+
+export default {
+  props: ['event', 'eventTicketTypes'],
+  methods: {
+    addToCart(eventTicketType, ticketCount) {
+      this.$emit('add-click', {...eventTicketType}, ticketCount)
+    },
+    removeFromCart(eventTicketType, ticketCount){
+      this.$emit('remove-click', {...eventTicketType}, ticketCount)
+    }
+  },
+  components: {
+    TicketType
+  }
+}
+</script>
+
