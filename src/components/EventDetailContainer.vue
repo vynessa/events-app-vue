e44cc2c51e1151f25b1e620876ae2d4b35c90e42
Vanessa Ejikeme <vanessa.ejikeme@gmail.com>
2020-07-26 16:03:24 +0100

    Fixes to the cart management and styles throughout the app - Fix addition and deletion of cart items - Modify checkmark styles - Implement order creation

--- src/components/EventDetailContainer.vue
+++ src/components/EventDetailContainer.vue
@@ -5,13 +5,13 @@
       :showRegisterModal="showRegisterModal"
       :toggleModal="toggleRegisterModal"
     >
-      <div v-if="formSubmitted">
+      <div v-if="formSubmitted" class="register-success-wrapper">
         <checkmark></checkmark>
         <p>
           You have successfully registered for {{event.name}}
         </p>
       </div>
-      <div v-else class="register-wrapper">
+      <div v-else class="register-form-wrapper">
         <h3 class="register-header-text">Register for free</h3>
         <hr class="register-horizontal-line">
         <FormulateForm  
@@ -22,48 +22,50 @@
       </div>
     </modal>
 
-    <loader v-if="loading" :loading="loading"></loader>
-    <div v-else class="event-detail">
-      <div class="event-detail__info">
-        <div class="event-detail__info-main">
-          <h6 class="event-detail__info-header">{{displayParsedDate(event.start_time)}}</h6>
-          <h4 class="event-detail__info-title">{{event.name}}</h4>
-          <em class="event-detail__info-description">{{event.description}}</em>
-          <p class="event-detail__info-price">N5,000 - N1,000,000</p>
-          <button v-on:click="handleBtnClick(true)" class="event-detail__register-btn btn-yellow">{{event.is_free ? "Register for free": "Buy Tickets"}}</button>
-        </div>
+    <main-container>  
+      <loader v-if="loading" :loading="loading"></loader>
+      <div v-else class="event-detail">
+        <div class="event-detail__info">
+          <div class="event-detail__info-main">
+            <h6 class="event-detail__info-header">{{displayParsedDate(event.start_time)}}</h6>
+            <h4 class="event-detail__info-title">{{event.name}}</h4>
+            <em class="event-detail__info-description">{{event.description}}</em>
+            <p class="event-detail__info-price">N5,000 - N1,000,000</p>
+            <button v-on:click="handleBtnClick(event.is_free)" class="event-detail__register-btn btn-yellow">{{event.is_free ? "Register for free": "Buy Tickets"}}</button>
+          </div>
 
-        <div class="event-detail__info-main">
-          <img class="event-detail__info-img" :src="event.image || '../assets/anthony-unsplash.jpg'">
-        </div>
-      </div>
-
-      <hr class="horizontal-line">
-
-      <div class="event-detail__info">
-        <div class="event-detail__info-extra">
-          <h6 class="event-detail__info-header">Venue</h6>
-          <h4 class="event-detail__info-text">{{event.venue}}</h4>
-
-          <div class="event-detail__directions">
-            <font-awesome-icon :icon="['far', 'map']" size="1x" :style="{ color: '#F5A623' }" />
-            <a href="" class="event-detail__directions-links">
-              View map for directions
-            </a>
+          <div class="event-detail__info-main">
+            <img class="event-detail__info-img" :src="event.image || '../assets/anthony-unsplash.jpg'">
           </div>
         </div>
-        <div class="event-detail__info-extra">
-          <h6 class="event-detail__info-header">Date and time</h6>
-          <h4 class="event-detail__info-text">{{displayParsedDateTime(event.start_time)}}</h4>
 
-          <h6 class="event-detail__info-header">Social links</h6>
-          <div class="event-detail__social-links" v-if="event.social_links && event.social_links.length > 0"> 
-            <!-- <p v-for="(socialLink, index)in event.social_links" :key="index" class="navbar-links">{{socialLink}}</p> -->
-          </div> 
-          <p class="event-detail__info-text" v-else>N/A</p>
+        <hr class="horizontal-line">
+
+        <div class="event-detail__info">
+          <div class="event-detail__info-extra">
+            <h6 class="event-detail__info-header">Venue</h6>
+            <h4 class="event-detail__info-text">{{event.venue}}</h4>
+
+            <div class="event-detail__directions">
+              <font-awesome-icon :icon="['far', 'map']" size="1x" :style="{ color: '#F5A623' }" />
+              <a href="" class="event-detail__directions-links">
+                View map for directions
+              </a>
+            </div>
+          </div>
+          <div class="event-detail__info-extra">
+            <h6 class="event-detail__info-header">Date and time</h6>
+            <h4 class="event-detail__info-text">{{displayParsedDateTime(event.start_time)}}</h4>
+
+            <h6 class="event-detail__info-header">Social links</h6>
+            <div class="event-detail__social-links" v-if="event.social_links && event.social_links.length > 0"> 
+              <!-- <p v-for="(socialLink, index)in event.social_links" :key="index" class="navbar-links">{{socialLink}}</p> -->
+            </div> 
+            <p class="event-detail__info-text" v-else>N/A</p>
+          </div>
         </div>
       </div>
-    </div>
+    </main-container>  
   </div>
 </template>
 
